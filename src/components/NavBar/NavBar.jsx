6fa5cb1c017169b6c719ed3e9eb48e6c2472bc00
IsaLjungdahl <isa.ljungdahl@gmail.com>
2021-09-13 13:30:21 +0200

    responsive navbar, burger button appears < 600px

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import Hamburger from "hamburger-react";
-import "./NavStyle.css";
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
-  const [showLinks, setShowLinks] = useState(true);
+  const [showLinks, setShowLinks] = useState(false);
   return (
-    <div className="navbar">
-      <div className="leftside">
+    <div className={styles.navbar}>
+      <div className={styles.leftside}>
         <h1>Logo</h1>
       </div>
-      <div className="rightside">
-        <div className="links" id={showLinks ? "hidden" : ""}>
+      <div className={styles.rightside}>
+        <div className={styles.links} id={showLinks ? "hidden" : ""}>
           <Link to="/">Home</Link>
 
           <Link to="/appetizer">Appetizers</Link>
@@ -21,7 +21,8 @@ const NavBar = () => {
 
           <Link to="/dessert">Desserts</Link>
         </div>
-        <div className="hamburger">
+
+        <div className={styles.hamburger}>
           <Hamburger toggled={showLinks} toggle={setShowLinks} />
         </div>
       </div>
